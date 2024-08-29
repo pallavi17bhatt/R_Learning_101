7515c87cda32af220ca1c6fe775cabb0b8b838d3
pallavibhatt <pallavibhatt@pallavibhatts-MacBook-Pro.local>
2024-08-29 13:48:48 +0530

    First R Code

--- HelloWorld.R
+++ HelloWorld.R
@@ -1,1 +1,25 @@
 print("HelloWorld")
+
+#Defining Variable "firstvar" and assigning value "foo"to it.
+
+firstvar<-"foo"
+#viewing  the value of the variable
+firstvar
+
+#R is case sensitive, try to run this line, it will show error
+#Firstvar 
+
+#Left Assignment in R
+x<-20
+x
+
+#Right Assignment in R
+20->x
+x
+
+#Assignment using equal sign
+x=20
+x
+#Assignment can also be done using assign() function
+assign("x",20)
+x
